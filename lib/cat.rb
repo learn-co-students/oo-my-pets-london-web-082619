52d9d33e503eae6e97e2bfa8f28b81c0bce6c4e8
Steve Alex <steve.alex@hotmail.co.uk>
2019-08-29 22:00:02 +0100

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,26 @@
+require_relative './owner'
+
+
 class Cat
-  # code goes here
+
+  @@all = []
+
+  attr_reader :name, :owner
+  attr_accessor :mood
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def owner=(new_owner)
+    @owner = new_owner
+  end
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
+require_relative './owner'
+
 class Dog
-  # code goes here
+
+  @@all = []
+
+  attr_reader :name, :owner
+  attr_accessor :mood
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def owner=(new_owner)
+    @owner = new_owner
+  end
+
 end
