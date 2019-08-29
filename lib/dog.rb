5a2c7d3823a78598914b413114d6cdcbaa7e1eaf
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-08-29 21:05:53 +0100

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
-  # code goes here
+  
+  @@all = []
+
+  attr_accessor :mood, :owner
+  attr_reader :name
+
+  def initialize(name, owner) #Dog.new
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def dogs #owner.dogs
+    #returns a a collection of all the dogs that belong to the owner
+    Dog.all.select { |dog| dog.owner == self }
+  end
+
 end
